49d16f4df9a94b837b5162ff3d6e21c1939fc683
jose rickardo <hei.rickardo1@gmail.com>
2025-11-16 00:27:43 +0300

    add the basics of my project

---
+++ src/component/Navbar.tsx
@@ -0,0 +1,9 @@
+import '../App.css'
+function Navbar() {
+    return (
+        <div className="bg-red text-3xl font-bold underline">
+            hello
+        </div>
+    )
+};
+export default Navbar;
