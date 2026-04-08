d4de31ee230d27b7a1080963869aea53ee1d5e63
jose rickardo <hei.rickardo1@gmail.com>
2026-04-08 17:49:29 +0300

    ajout d'une rectifiacation pour mon transporter

--- server/index.js
+++ server/index.js
@@ -22,15 +22,16 @@ app.post('/send-email', async (req, res) => {
   const { name, email, subject, message } = req.body;
 
   try {
-    // Config transporteur
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
+    //transporteur
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
     // Email
     const mailOptions = {
       from: `"${name}" <${process.env.EMAIL_USER}>`,
