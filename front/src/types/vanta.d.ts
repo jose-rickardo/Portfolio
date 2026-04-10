88968ac0334e149f4274aaf512a1c2b42a515ed5
jose rickardo <hei.rickardo1@gmail.com>
2026-04-10 22:06:36 +0300

    ajout du corection pour build

--- front/src/types/vanta.d.ts
+++ front/src/types/vanta.d.ts
@@ -1,4 +1,3 @@
-
 declare module 'vanta/dist/vanta.net.min';
 declare module 'vanta/dist/vanta.birds.min';
 declare module 'vanta/dist/vanta.waves.min';
@@ -11,4 +10,5 @@ declare module 'vanta/dist/vanta.halo.min';
 declare module 'vanta/dist/vanta.rings.min';
 declare module 'vanta/dist/vanta.ripple.min';
 declare module 'vanta/dist/vanta.topology.min';
+
 declare module 'vanta/dist/vanta.trunk.min';
